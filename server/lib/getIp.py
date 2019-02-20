aee40a8693bae49437a50b94fcfbb1b1f2b7bb75
Frank Doyle <fdoyl001@gmail.com>
2019-02-19 18:04:59 -0600

    remove broken logger statements

--- server/lib/getIp.py
+++ server/lib/getIp.py
@@ -9,11 +9,10 @@ def get_network_interface_ip_address(interface='eth0'):
     """
     while True:
         if NETWORK_INTERFACE not in ni.interfaces():
-            logger.error('Could not find interface %s.' % (interface,))
+            print('Could not find interface %s.' % (interface,))
             exit(1)
         interface = ni.ifaddresses(interface)
         if (2 not in interface) or (len(interface[2]) == 0):
-            logger.warning('Could not find IP of interface %s. Sleeping.' % (interface,))
-            sleep(60)
+            print('Could not find IP of interface %s. Sleeping.' % (interface,))
             continue
         return interface[2][0]['addr']
